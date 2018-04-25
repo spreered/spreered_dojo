cc9a184c046e315416877617b2a01a23aa6b2d91
spreered <spreered@gmail.com>
2018-04-25 11:55:52 +0800

    add user show edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  def show
+  end
+  def edit
+  end
+  def update
+    if @user.update!(user_params)
+      flash[:notice] = '更新成功'
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = '更新失敗'
+      render edit_user_path(@user)
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name,:description,:avatar)
+  end
 end
