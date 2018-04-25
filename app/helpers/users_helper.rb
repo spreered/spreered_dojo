ae795c97ca2efe55cf8581c12a40f40aef52913f
spreered <spreered@gmail.com>
2018-04-25 16:14:24 +0800

    modify post show style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def rect_avatar(avatar)
+    content_tag :div, class: 'imgbox' do
+      content_tag :div, class: 'imgbox-inner' do
+        content_tag :div, class: 'imgbox-fit' do
+          image_tag avatar
+        end
+      end
+    end
+  end
 end
