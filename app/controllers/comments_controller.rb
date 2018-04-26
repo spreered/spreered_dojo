47170f533eae195cce25bf2370e158715f6412ff
spreered <spreered@gmail.com>
2018-04-27 00:12:49 +0800

    add comment edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only:[:edit,:update,:destroy]
   def create
     post = Post.find(params[:post_id])
     @comment = post.comments.build(comment_params)
@@ -13,16 +14,21 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    render json:{content: @comment.content }
   end
 
   def update
+    @comment.update!(comment_params)
+    render json: { id: @comment.id, content: @comment.content }
   end
 
   def destroy
   end
 
   private
-
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
   def comment_params
     params.require(:comment).permit(:content)
   end 
