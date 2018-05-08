c02faade6939a34528ee209bf10ac562edbeafa7
spreered <spreered@gmail.com>
2018-05-09 00:13:55 +0800

    fix bug post order

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,6 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @categories = Category.all
     @q = @category.posts.published.all_user.or(@category.posts.published.friend_post(current_user)).or(@category.posts.published.myself_post(current_user)).ransack(params[:q])
-    @posts = @q.result(distinct: true).order(id: :desc).page(params[:page]).per(20)
+    @posts = @q.result(distinct: true).order(:id).page(params[:page]).per(20)
   end
 end
